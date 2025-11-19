c3556ab0a0ca020a02acb9736e6e457ea1fb319a
Lina Hernandez <linaheva@hotmail.com>
2025-11-18 22:25:02 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = 'https://cnt-d2a3735e-236d-4883-a48c-2387bfb0a7a5.containerhub.tripleten-services.com'
+CREATE_USER_PATH = "/api/v1/users/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
---
+++ data.py
@@ -0,0 +1,8 @@
+headers = {
+    "Content-Type": "application/json"
+}
+user_body = {
+    "firstName": "Max",
+    "phone": "+11234567890",
+    "address": "123 Elm Street, Hilltop"
+}
